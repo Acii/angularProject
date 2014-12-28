7f0ffa94b238effb0aea2be94faa03d4142115fe
Asen <Acii@users.noreply.github.com>
2014-12-28 19:00:26 +0200

    bug fixed

--- script/app.js
+++ script/app.js
@@ -1,18 +1,23 @@
 var app = angular.module('adsApp', ['ngResource', 'ngRoute'])
-	.config(function($routeProvider) {
-		
-		$routeProvider.when('/home', {
-			templateUrl: 'themplates/home.html',
-			controller: 'HomeController'
-		});
-		
-		$routeProvider.when('/login', {
-			templateUrl: 'themplates/login.html',
-			controller: 'loginController'	
-		});
-		
-		$routeProvider.when('/register', {
-			templateUrl: 'themplates/register.html',
-			controller: 'registerController'
-		});
+.config(function ($routeProvider) {
+	$routeProvider.when('/home', {
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
 	});
+});
+// var app = angular.module('softUniApp', ['ngResource', 'ngRoute'])
+// .config(function ($routeProvider) {
+	// $routeProvider.when('/newAd', {
+		// templateUrl: 'templates/newAd.html',
+		// controller: 'NewAdController'
+	// });
+	// $routeProvider.when('/ads', {
+		// templateUrl: 'templates/allAds.html',
+		// controller: 'AllAdsController'
+	// });
+	// $routeProvider.when('/ads/:adId', {
+		// test: 'Pesho',
+		// templateUrl: 'templates/adDetails.html',
+		// controller: 'AdDetailsController'
+	// });
+// });
--- script/controller/homeController.js
+++ script/controller/homeController.js
@@ -1,9 +1,25 @@
-app.controller('HomeController', function($scope, adsData, $log) {
-	adsData.getAll()
+app.controller('HomeController', function ($scope, $route, $log, adsData) {
+	adsData.getAds()
 		.$promise
 		.then(function (data) {
 			$scope.data = data;
 		}, function (error) {
 			$log.error(error);
-		})
+		});
 });
+
+
+
+// app.controller('AllAdsController', function($scope, $route, $log, adsData) {
+	// adsData.getAll()
+		// .$promise
+		// .then(function (data) {
+			// $scope.data = data;
+		// }, function (error) {
+			// $log.error(error);
+		// });
+// 
+	// $scope.reload = function () {
+		// $route.reload();
+	// }
+// });
